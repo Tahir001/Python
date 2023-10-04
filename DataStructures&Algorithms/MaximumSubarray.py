4cc115d7fdf5440beca9324d0f8c0a0b20276764
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-10-04 15:35:43 -0400

    Update

--- DataStructures&Algorithms/MaximumSubarray.py
+++ DataStructures&Algorithms/MaximumSubarray.py
@@ -15,8 +15,10 @@ def maxSubArray(nums):
         curr_sum = max(0, curr_sum)
         curr_sum += num
         max_sum = max(curr_sum, max_sum)
+
     return max_sum
 
+    # OTHER APPROACHES BELOW. 
     # O(N**2) time complexity 
     # Brute Force
     # max_sum = nums[0]
@@ -53,27 +55,3 @@ def maxSubArray(nums):
 array = [4,-1,2,-7,3,4]
 maxSubArray(array)
 print(maxSubArray(array))
-        
-
-    # Kadane's Algorithm - O(N) time complexity
-    # Goal is to do 1 pass
-    # Essentially keeping a running total and if its less than 0, disregard it and start again
-    # max_sum = nums[0]
-    # curr_sum = 0
-
-    # for num in nums:
-    #     # Basically set it equal to 0 if it's current running total is negative
-    #     curr_sum = max(0, curr_sum)
-    #     curr_sum += num
-    #     max_sum = max(curr_sum, max_sum)
-    # return max_sum
-
-    # O(N**2) time complexity 
-    # Brute Force
-    # max_sum = nums[0]
-    # for i in range(len(nums)):
-    #     curr_sum = 0
-    #     for j in range(i, len(nums)):
-    #         curr_sum += nums[j]
-    #         max_sum = max(curr_sum, max_sum)
-    # return max_sum 
