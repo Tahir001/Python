cb72468971b62208841b790d21ce287189289dac
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-10-16 00:50:34 -0400

    Completed MaximumSumCircularSubarray

--- DataStructures&Algorithms/MaximumSumCircularSubarray.py
+++ DataStructures&Algorithms/MaximumSumCircularSubarray.py
@@ -5,6 +5,25 @@ def maxSubarraySumCircular(nums):
     """
     # LC Medium: https://leetcode.com/problems/maximum-sum-circular-subarray/
     # Solution 1 for max subarray - using kadanes algorithm
-    # The goal is to keep track of total sum and total min
+    currMax, globMax = 0, nums[0]
+    currMin, globMin = 0, nums[0]
+    total = 0
+
+    for n in nums:
+        # Update total sum of the array
+        total += n
+
+        # Update current values 
+        currMax = max(currMax + n, n)
+        currMin = min(currMin + n, n)
+
+        # Update globals 
+        globMax = max(globMax, currMax)
+        globMin = min(globMin, currMin)
+
     
+    return max(globMax, (total - globMin)) if globMax > 0 else globMax
+
+
+
     
