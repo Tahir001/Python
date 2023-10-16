51b136907b1e20fb5eeef8a007abe6e41bf20850
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-10-15 23:58:29 -0400

    Use Kadanes Algorithm

--- DataStructures&Algorithms/MaximumSumCircularSubarray.py
+++ DataStructures&Algorithms/MaximumSumCircularSubarray.py
@@ -4,5 +4,6 @@ def maxSubarraySumCircular(nums):
     :rtype: int
     """
     # LC Medium: https://leetcode.com/problems/maximum-sum-circular-subarray/
+    # Solution 1 for max subarray - using kadanes algorithm
     
     
