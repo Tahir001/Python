1ba403f84168915b5fcfdbb8a33f316b31053b63
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-10-15 23:59:00 -0400

    Update

--- DataStructures&Algorithms/MaximumSumCircularSubarray.py
+++ DataStructures&Algorithms/MaximumSumCircularSubarray.py
@@ -5,5 +5,6 @@ def maxSubarraySumCircular(nums):
     """
     # LC Medium: https://leetcode.com/problems/maximum-sum-circular-subarray/
     # Solution 1 for max subarray - using kadanes algorithm
+    # The goal is to keep track of total sum and total min
     
     
